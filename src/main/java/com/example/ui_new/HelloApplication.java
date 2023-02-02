1317ec774da38b437cbdd3c9d8e776d22af41461
T470@DESKTOP-6JU3RJD <kalixyanau2002hh4@gmail.com>
2023-02-02 12:52:23 +0700

    cap nhat Trieu Dai Lich Su view

--- src/main/java/com/example/ui_new/HelloApplication.java
+++ src/main/java/com/example/ui_new/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.ui_new;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.media.Media;
@@ -13,12 +14,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         // nào Build thì bỏ comment ra để phát nhạc
-            Media media = new Media(Paths.get("src/main/resources/assets/music/HaoKhiVietNam.mp3").toUri().toString());
+         /*   Media media = new Media(Paths.get("src/main/resources/assets/music/HaoKhiVietNam.mp3").toUri().toString());
             MediaPlayer mediaPlayer = new MediaPlayer(media);
             mediaPlayer.setAutoPlay(true);
             mediaPlayer.setMute(false);
             mediaPlayer.play();
-
+*/
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 
