e1bef7cef7fae6ac9b64e040e5b8cc24b4778333
nguyennam5902 <nguyenhainam05092002@gmail.com>
2023-02-03 08:15:57 +0700

    add com.google.gson to read json

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,9 +4,11 @@ module com.example.ui_new {
     requires javafx.media;
     requires javafx.web;
     requires json.simple;
+    requires com.google.gson;
 
 
     opens com.example.ui_new to javafx.fxml;
+    opens com.example.objects to com.google.gson;
     exports com.example.ui_new;
     //opens com.example.ui_new.Controller to javafx.fxml;
 }
